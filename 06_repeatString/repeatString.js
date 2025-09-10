f66af507201baddeee2e4e9c23d3a1e0390e297b
nir-web <nir.web23@gmail.com>
2025-09-11 02:18:19 +0530

    Complete 6th and 7th excercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+    let finalString = '';
+    Math.floor(repetitions);
+    if (repetitions >= 0){
+        for (let i = 0; i < repetitions; i++){
+            finalString += string;
+        }
+        return finalString;
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string
+        .split(' ')
+        .reverse()
+        .map(word => word.split('').reverse().join(''))
+        .join(' ');
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
